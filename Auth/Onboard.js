431b94f9791fc03e4e874bc1d998a8128a717763
gaurav-1302 <gaurav130202chaudhary@gmail.com>
2023-10-27 11:43:28 +0530

    Google Fit 1

--- Auth/Onboard.js
+++ Auth/Onboard.js
@@ -1,52 +1,116 @@
 import React, { useCallback, useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
+import { View, Text, StyleSheet, Image, TouchableOpacity, Platform, Button } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { LinearGradient } from 'expo-linear-gradient'
 import { useFonts } from 'expo-font';
 import * as SplashScreen from 'expo-splash-screen';
-import * as Google from 'expo-auth-session/providers/google'
+import * as Google from 'expo-auth-session/providers/google';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as AuthSession from 'expo-auth-session';
 
 const banner = require('../assets/images/banner.jpg')
 
-export default function Onboard() {
+export default function Onboard({ navigation }) {
 
-    const [accessToken, setAccessToken] = useState();
-    const [userInfo, setUserInfo] = useState();
+    const [auth, setAuth] = useState();
+    const [requireRefresh, setRequireRefresh] = useState(false);
+
+    const scopes =
+        [
+            'https://www.googleapis.com/auth/fitness.activity.read',
+            'https://www.googleapis.com/auth/fitness.activity.write',
+            'https://www.googleapis.com/auth/fitness.location.read',
+            'https://www.googleapis.com/auth/fitness.location.write',
+            'https://www.googleapis.com/auth/fitness.body.read',
+            'https://www.googleapis.com/auth/fitness.body.write',
+            'https://www.googleapis.com/auth/fitness.nutrition.read',
+            'https://www.googleapis.com/auth/fitness.nutrition.write',
+            'https://www.googleapis.com/auth/fitness.blood_pressure.read',
+            'https://www.googleapis.com/auth/fitness.blood_pressure.write',
+            'https://www.googleapis.com/auth/fitness.blood_glucose.read',
+            'https://www.googleapis.com/auth/fitness.blood_glucose.write',
+            'https://www.googleapis.com/auth/fitness.oxygen_saturation.read',
+            'https://www.googleapis.com/auth/fitness.oxygen_saturation.write',
+            'https://www.googleapis.com/auth/fitness.body_temperature.read',
+            'https://www.googleapis.com/auth/fitness.body_temperature.write',
+            'https://www.googleapis.com/auth/fitness.reproductive_health.read',
+            'https://www.googleapis.com/auth/fitness.reproductive_health.write',
+        ]
 
     const [request, response, promptAsync] = Google.useAuthRequest({
         androidClientId: "695485437157-1cjmk7sk4pmgi9n4sum5j6s79c5b7ckv.apps.googleusercontent.com",
         iosClientId: "695485437157-2gn77vo13navtpab6ve8331s1hdsqhu6.apps.googleusercontent.com",
         expoClientId: "695485437157-ejphndvhgn5gc29pass6tr0cb3r86dfr.apps.googleusercontent.com",
+        scopes: scopes
     })
 
     useEffect(() => {
         if (response?.type === "success") {
-            setAccessToken(response.authentication?.accessToken);
+            setAuth(response.authentication);
+
+            const persistAuth = async () => {
+                await AsyncStorage.setItem("auth", JSON.stringify(response.authentication));
+            };
+            persistAuth();
         }
     }, [response]);
 
-    const getUserData = async () => {
-        let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
-            headers: { Authorization: `Bearer ${accessToken}` }
-        });
+    useEffect(() => {
+        const getPersistedAuth = async () => {
+            const jsonValue = await AsyncStorage.getItem("auth");
+            if (jsonValue != null) {
+                const authFromJson = JSON.parse(jsonValue);
+                setAuth(authFromJson);
+                console.log(authFromJson);
+                setRequireRefresh(!AuthSession.TokenResponse.isTokenFresh({
+                    expiresIn: authFromJson.expiresIn,
+                    issuedAt: authFromJson.issuedAt
+                }));
+            }
+        };
+        getPersistedAuth();
+    }, []);
 
-        userInfoResponse.json().then(data => {
-            setUserInfo(data);
-        })
-    };
-
-    const showUserData = () => {
-        if (userInfo) {
-            return (
-                <View>
-                    <Text>Welcome {userInfo.name}</Text>
-                    <Text>Email {userInfo.email}</Text>
-                </View>
-            )
+    const getClientId = () => {
+        if (Platform.OS === 'ios') {
+            return "695485437157-2gn77vo13navtpab6ve8331s1hdsqhu6.apps.googleusercontent.com";
+        } else if (Platform.OS === 'android') {
+            return "695485437157-1cjmk7sk4pmgi9n4sum5j6s79c5b7ckv.apps.googleusercontent.com";
+        } else {
+            console.log("Invalid platform - not handled");
         }
     }
 
+    const refreshToken = async () => {
+        const clientId = getClientId();
+        const tokenResult = await AuthSession.refreshAsync({
+            clientId: clientId,
+            refreshToken: auth.refreshToken
+        }, {
+            tokenEndpoint: "https://www.googleapis.com/oauth2/v4/token"
+        });
 
+        tokenResult.refreshToken = auth.refreshToken;
+
+        setAuth(tokenResult);
+        await AsyncStorage.setItem("auth", JSON.stringify(tokenResult));
+        setRequireRefresh(false);
+    };
+
+    if (requireRefresh) {
+        refreshToken();
+    }
+
+    useEffect(() => {
+        const checkAuthState = async () => {
+            const jsonValue = await AsyncStorage.getItem("auth");
+            console.log("Auth State", jsonValue);
+            if (jsonValue != null) {
+                navigation.navigate('Home');
+            }
+        }
+        checkAuthState();
+    }, [auth]);
 
     const [fontsLoaded] = useFonts({
         'PollerOne-Regular': require('../assets/fonts/PollerOne-Regular.ttf'),
@@ -72,16 +136,15 @@ export default function Onboard() {
                 <Image source={banner} style={styles.banner} />
             </View>
             <View style={styles.main}>
-                {showUserData()}
                 <LinearGradient style={styles.groupChild} locations={[0, 1]} colors={['#8b78ff', '#5451d6']} useAngle={true} angle={135.96} />
                 <Text style={styles.brand}>FitPhy</Text>
                 <Text style={styles.subHead}>Maximize Your Athletic Potential</Text>
                 <Text style={styles.subHead2}>Onboard to unlock AI-powered coaching, real-time feedback, and a community of athletes. Get started!</Text>
                 <TouchableOpacity activeOpacity={0.9} style={styles.base}
-                    onPress={accessToken ? getUserData : () => promptAsync({ useProxy: true, showInRecents: true })}
+                    onPress={() => promptAsync({ useProxy: true, showInRecents: true })}
                 >
                     <LinearGradient style={styles.base1} locations={[0, 1]} colors={['#8b78ff', '#5451d6']} useAngle={true} angle={135.96}>
-                        <Text style={styles.buttonText}>{accessToken ? "Get Data" : "Login"}</Text>
+                        <Text style={styles.buttonText}>Continue</Text>
                     </LinearGradient>
                 </TouchableOpacity>
             </View>
@@ -165,6 +228,6 @@ const styles = StyleSheet.create({
         fontFamily: 'OpenSans-Bold',
         color: "#fff",
         textAlign: "center",
-    }
+    },
 })
 
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,10 +1,133 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet, Button } from 'react-native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import * as AuthSession from 'expo-auth-session';
 
-export default function Home() {
+export default function Home({ navigation }) {
+
+    const [userInfo, setUserInfo] = useState();
+    const [auth, setAuth] = useState();
+    const [stepCount, setStepCount] = useState(0);
+
+    const logout = async () => {
+        try {
+            await AuthSession.revokeAsync({
+                token: auth.accessToken
+            }, {
+                revocationEndpoint: "https://oauth2.googleapis.com/revoke"
+            });
+        } catch (error) {
+            alert("Error", error)
+        }
+
+        setAuth(undefined);
+        setUserInfo(undefined);
+        await AsyncStorage.removeItem("auth");
+        navigation.navigate('Onboard');
+    };
+
+    useEffect(() => {
+        const getAuthData = async () => {
+            const jsonValue = await AsyncStorage.getItem("auth");
+            if (jsonValue != null) {
+                const authFromJson = JSON.parse(jsonValue);
+                setAuth(authFromJson);
+                console.log(authFromJson);
+            }
+            else {
+                alert("Error in fetching access token");
+                logout();
+            }
+        }
+        getAuthData();
+    }, [])
+
+    const getUserData = async () => {
+        let userInfoResponse = await fetch("https://www.googleapis.com/userinfo/v2/me", {
+            headers: { Authorization: `Bearer ${auth.accessToken}` }
+        });
+
+        userInfoResponse.json().then(data => {
+            console.log(data);
+            setUserInfo(data);
+        });
+    };
+
+    const getGoogleFitData = async (authToken) => {
+        const endpoint = "https://www.googleapis.com/fitness/v1/users/me/dataset:aggregate";
+        const dataType = "derived:com.google.step_count.delta:com.google.android.gms:estimated_steps";
+        const startTimeMillis = Date.now() - 24 * 60 * 60 * 1000; // Start time (e.g., 24 hours ago)
+        const endTimeMillis = Date.now(); // End time (current time)
+
+        const request = {
+            aggregateBy: [
+                {
+                    dataSourceId: dataType,
+                },
+            ],
+            bucketByTime: {
+                durationMillis: 86400000
+            },
+            startTimeMillis,
+            endTimeMillis,
+        };
+
+        try {
+            const response = await fetch(endpoint, {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${authToken}`,
+                    "Content-Type": "application/json;encoding=utf-8",
+                },
+                body: JSON.stringify(request),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                if (response.bucket && response.bucket.length > 0) {
+                    const dataset = response.bucket[0].dataset;
+                    if (dataset && dataset.length > 0) {
+                        const stepData = dataset[0].point;
+                        if (stepData && stepData.length > 0) {
+                            const stepValue = stepData[0].value[0].intVal;
+                            setStepCount(stepValue);
+                        }
+                    }
+                }
+                // Process and handle the Google Fit data as needed.
+            } else {
+                console.log(response);
+            }
+        } catch (error) {
+            console.log("Error", error);
+        }
+    };
+
+    useEffect(() => {
+        if (auth) {
+            getUserData();
+            console.log(auth.accessToken);
+        }
+    }, [auth])
+
+
+
     return (
-        <View>
+        <View style={styles.container}>
             <Text>Home Screen</Text>
+            <Text>{userInfo?.email}</Text>
+            <Text>{stepCount}</Text>
+            <Button title='Logout' onPress={() => logout()} />
+            <Button title='Get Fitness Data' onPress={() => getGoogleFitData(auth.accessToken)} />
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
+})
